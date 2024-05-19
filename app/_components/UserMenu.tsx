3beccbd9fd2ec362d9429c7a497210aae6b7cd7a
abr-ya <gamer05@list.ru>
2024-05-19 17:58:02 +0300

    feat: Add UserMenu component

--- app/_components/UserMenu.tsx
+++ app/_components/UserMenu.tsx
@@ -1,15 +1,72 @@
-// import { useRouter } from "next/navigation";
-// import Link from "next/link";
+"use server";
 
-const UserMenu = () => {
-  // const router = useRouter();
+import Link from "next/link";
+import Image from "next/image";
+import { MenuIcon } from "lucide-react";
 
-  // const onLogout = () => {
-  //   // logout
-  //   router.refresh();
-  // };
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-  return <div>UserMenu</div>;
+const UserMenu = async () => {
+  const user = { id: 1 }; // await getUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
+          <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
+          <Image
+            // todo: user logo
+            src="/no-avatar.jpg"
+            className="rounded-full h-8 w-8 hidden lg:block"
+            alt="Image of the user"
+            width={100}
+            height={100}
+          />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[200px]">
+        {user ? (
+          <>
+            <DropdownMenuItem>
+              <form className="w-full">
+                <button type="submit" className="w-full text-start">
+                  Airbnb your Home
+                </button>
+              </form>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/my-homes" className="w-full">
+                My Listings
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/favorites" className="w-full">
+                My Favorites
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/reservations" className="w-full">
+                My Reservations
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </>
+        ) : (
+          <>
+            <DropdownMenuItem>RegisterLink</DropdownMenuItem>
+            <DropdownMenuItem>LoginLink</DropdownMenuItem>
+          </>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default UserMenu;
