4754e94ff935c3e416e2bf3e996546b3ff4e22fa
abr-ya <gamer05@list.ru>
2024-06-07 08:44:56 +0300

    feat: Add saving Home data to database

--- app/actions.ts
+++ app/actions.ts
@@ -10,15 +10,21 @@ export async function createHomeAction({ userId }: { userId: string }) {
     orderBy: { createdAT: "desc" },
   });
 
-  if (data === null) {
+  // если нет данных или если заполнен шаг 3 - создаём новый объект
+  if (data === null || data.hasStep3) {
     console.log(`Create new Home for user ${userId} and go to step 1...`);
-    // const data = await prisma.home.create({ data: { userId } });
-    const data = { id: "khuihuhuih" };
+    const data = await prisma.home.create({ data: { userId } });
 
     return redirect(`/new/${data.id}/step-1`);
   } else {
     // todo: логика, если что-то уже записано в бд с этим id
-    return false;
+    if (!data.hasStep1) {
+      return redirect(`/create/${data.id}/step-1`);
+    } else if (!data.hasStep2) {
+      return redirect(`/create/${data.id}/step-2`);
+    } else if (!data.hasStep3) {
+      return redirect(`/create/${data.id}/step-3`);
+    } // вариант, где есть все 3 - создаём новый - выше)
   }
 }
 
@@ -27,17 +33,11 @@ export const saveCategoryAction = async (formData: FormData) => {
   const homeId = formData.get("homeId") as string;
 
   console.log(`Save to Home ${homeId} category ${category}`);
-
-  // const data = await prisma.home.update({
-  //   where: {
-  //     id: homeId,
-  //   },
-  //   data: {
-  //     category: categoryName,
-  //     hasStep1: true,
-  //   },
-  // });
-  // console.log("Home updated:", data);
+  const data = await prisma.home.update({
+    where: { id: homeId },
+    data: { category, hasStep1: true },
+  });
+  console.log("Home updated:", data);
 
   return redirect(`/new/${homeId}/step-2`);
 };
@@ -55,7 +55,13 @@ export const saveTextAction = async (formData: FormData) => {
 
   // todo: Upload Image
   console.log(`ToDo: upload Image ${imageFile}`);
-  const imageData = { path: imageFile };
+  // const { data: imageData } = await supabase.storage
+  //   .from("images")
+  //   .upload(`${imageFile.name}-${new Date()}`, imageFile, {
+  //     cacheControl: "2592000",
+  //     contentType: "image/png",
+  //   });
+  const imageData = { path: "https://www.buildi.com.au/wp-content/uploads/2023/06/double-storey-house-scaled.jpg" };
 
   // prepare data
   const updateData = {
@@ -67,16 +73,16 @@ export const saveTextAction = async (formData: FormData) => {
     roomCount: Number(roomNumber),
     bathroomCount: Number(bathroomsNumber),
 
-    photo: imageData?.path,
+    imageSrc: imageData?.path,
     hasStep2: true,
   };
 
   console.log(`Add to ${homeId} data: ${updateData}`);
-
-  // const data = await prisma.home.update({
-  //   where: { id: homeId },
-  //   data: updateData,
-  // });
+  const data = await prisma.home.update({
+    where: { id: homeId },
+    data: updateData,
+  });
+  console.log("Home updated:", data);
 
   return redirect(`/new/${homeId}/step-3`);
 };
@@ -85,22 +91,20 @@ export const saveCountryAction = async (formData: FormData) => {
   // todo: get it another way?
   const homeId = formData.get("homeId") as string;
   const country = formData.get("country") as string;
-  const pinLat = formData.get("pinLat") as string;
-  const pinLon = formData.get("pinLon") as string;
+  const pinLat = Number(formData.get("pinLat"));
+  const pinLon = Number(formData.get("pinLon"));
 
   console.log(`Add ${country} (${pinLat}, ${pinLon}) to ${homeId}`);
-
-  // const data = await prisma.home.update({
-  //   where: {
-  //     id: homeId,
-  //   },
-  //   data: {
-  //     country,
-  //     pinLat,
-  //     pinLon,
-  //     hasStep3: true,
-  //   },
-  // });
+  const data = await prisma.home.update({
+    where: { id: homeId },
+    data: {
+      country,
+      pinLat,
+      pinLon,
+      hasStep3: true,
+    },
+  });
+  console.log("Home updated:", data);
 
   return redirect("/");
 };
