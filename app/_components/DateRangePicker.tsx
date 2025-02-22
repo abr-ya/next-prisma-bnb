49266029e611e21409c29308c00367270eed3979
abr-ya <gamer05@list.ru>
2025-02-22 12:31:36 +0300

    feat: Add DateRangePicker component

---
+++ app/_components/DateRangePicker.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { DateRange, Range } from "react-date-range";
+import { FC, useState } from "react";
+import { eachDayOfInterval } from "date-fns";
+
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+import { IDateRange } from "../_interfaces/booking.interfaces";
+
+interface IDateRangePicker {
+  reservation?: IDateRange[];
+}
+
+const DateRangePicker: FC<IDateRangePicker> = ({ reservation }) => {
+  const [state, setState] = useState<Range>({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+
+  let disabledDates: Date[] = [];
+  reservation?.forEach((reservationItem) => {
+    const dateRange = eachDayOfInterval({
+      start: new Date(reservationItem.startDate),
+      end: new Date(reservationItem.endDate),
+    });
+
+    disabledDates = [...disabledDates, ...dateRange];
+  });
+  return (
+    <>
+      <input type="hidden" name="startDate" value={state && state.startDate ? state.startDate.toISOString() : ""} />
+      <input type="hidden" name="endDate" value={state && state.endDate ? state.endDate.toISOString() : ""} />
+      <DateRange
+        date={new Date()}
+        showDateDisplay={false}
+        rangeColors={["#FF5A5F"]}
+        ranges={[state]}
+        onChange={(item) => setState(item.selection)}
+        minDate={new Date()}
+        direction="vertical"
+        disabledDates={disabledDates}
+      />
+    </>
+  );
+};
+
+export default DateRangePicker;
