3667e8331bf7aca4085ef6bc95c262a111f04971
abr-ya <gamer05@list.ru>
2025-03-16 00:18:31 +0300

    fix: Fix Leaflet Demo Paris Map

--- app/(maps)/paris/page.tsx
+++ app/(maps)/paris/page.tsx
@@ -1,19 +1,10 @@
-import { data } from "./data";
-// import dynamic from "next/dynamic";
-// import { Skeleton } from "@/components/ui/skeleton";
+import ClientLazyMap from "./components/ClientLazyMap";
 
-const page = () => {
-  // const loading = () => <Skeleton className="h-[calc(100vh-200px)] w-full" />;
-  // const LazyMap = dynamic(() => import("./components/PlacesMap"), { loading });
-  console.log(data);
-
-  return (
-    <div>
-      <h2>Paris Demo</h2>
-      {/* <LazyMap places={data} /> */}
-      This page is temporary switch off in Next 15.
-    </div>
-  );
-};
+const page = () => (
+  <div>
+    <h2>Paris Demo</h2>
+    <ClientLazyMap />
+  </div>
+);
 
 export default page;
