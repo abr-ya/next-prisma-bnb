82f034247d5279e3e5beb6a145a10924ff80d7d4
abr-ya <gamer05@list.ru>
2025-03-29 19:03:32 +0300

    feature: Add Resume Editor page

--- app/resumes/_components/CreateResumeButton.tsx
+++ app/resumes/_components/CreateResumeButton.tsx
@@ -2,18 +2,23 @@
 
 import { Button } from "@/components/ui/button";
 import { PlusSquare } from "lucide-react";
+import Link from "next/link";
 
 interface CreateResumeButtonProps {
   canCreate: boolean;
 }
 
-export default function CreateResumeButton({ canCreate }: CreateResumeButtonProps) {
+const CreateResumeButton = ({ canCreate }: CreateResumeButtonProps) => {
   console.log(canCreate);
 
   return (
-    <Button className="mx-auto flex w-fit gap-2">
-      <PlusSquare className="size-5" />
-      New resume
+    <Button asChild className="mx-auto flex w-fit gap-2">
+      <Link href="/resumes/editor">
+        <PlusSquare className="size-5" />
+        New resume
+      </Link>
     </Button>
   );
-}
+};
+
+export default CreateResumeButton;
---
+++ app/resumes/editor/page.tsx
@@ -0,0 +1,25 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Metadata } from "next";
+
+import ClientResumeEditor from "./_components/ClientResumeEditor";
+
+interface IEditorPage {
+  searchParams: Promise<{ resumeId?: string }>;
+}
+
+export const metadata: Metadata = {
+  title: "Design your resume",
+};
+
+const EditorPage = async ({ searchParams }: IEditorPage) => {
+  const { resumeId } = await searchParams;
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  console.log(resumeId, user.id);
+
+  return <ClientResumeEditor />;
+};
+
+export default EditorPage;
