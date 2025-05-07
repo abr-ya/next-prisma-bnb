96126526f98de40a133a79848583c35c57acf394
abr-ya <gamer05@list.ru>
2025-05-07 21:28:23 +0300

    feature: Add Personal info form

--- app/resumes/editor/_components/ResumeForm.tsx
+++ app/resumes/editor/_components/ResumeForm.tsx
@@ -1,14 +1,15 @@
 import { IEditorForm } from "@/zod/resume.schema";
-import { FC } from "react";
+import { ComponentType, FC } from "react";
 import { Step1GeneralInfoForm } from ".";
+import Step2PersonalInfoForm from "./Step2PersonalInfoForm";
 
 interface IResumeForm extends IEditorForm {
   stepKey: string;
 }
 
 const ResumeForm: FC<IResumeForm> = ({ stepKey, ...props }) => {
-  console.log(stepKey);
-  let CurrentForm: React.ComponentType<IEditorForm> | null;
+  console.log("ResumeForm", stepKey);
+  let CurrentForm: ComponentType<IEditorForm> | null;
 
   const EmptyComponent = () => <p>Error key or component not finished yet...</p>;
 
@@ -17,9 +18,18 @@ const ResumeForm: FC<IResumeForm> = ({ stepKey, ...props }) => {
       CurrentForm = Step1GeneralInfoForm;
       break;
     case "personal-info":
-      CurrentForm = null;
+      CurrentForm = Step2PersonalInfoForm;
       break;
     case "work-experience":
+      CurrentForm = null;
+      break;
+    case "education":
+      CurrentForm = null;
+      break;
+    case "skills":
+      CurrentForm = null;
+      break;
+    case "summary":
       CurrentForm = null;
       break;
     default:
---
+++ app/resumes/editor/_components/Step2PersonalInfoForm.tsx
@@ -0,0 +1,131 @@
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { IEditorForm, personalInfoSchema, PersonalInfoValues } from "@/zod/resume.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
+
+const Step2PersonalInfoForm = ({ resumeData, setResumeData }: IEditorForm) => {
+  const form = useForm<PersonalInfoValues>({
+    resolver: zodResolver(personalInfoSchema),
+    defaultValues: {
+      firstName: resumeData.firstName || "",
+      lastName: resumeData.lastName || "",
+      jobTitle: resumeData.jobTitle || "",
+      city: resumeData.city || "",
+      country: resumeData.country || "",
+      phone: resumeData.phone || "",
+      email: resumeData.email || "",
+    },
+  });
+
+  const firstName = form.watch("firstName");
+  const lastName = form.watch("lastName");
+  const email = form.watch("email");
+  const photo = form.watch("photo");
+
+  useEffect(() => {
+    const asyncValidationAndSave = async () => {
+      const isValid = await form.trigger();
+      // @ts-expect-error prev?!
+      if (isValid) setResumeData((prev) => ({ ...prev, firstName, lastName, email }));
+    };
+    console.log(firstName, lastName, email, photo);
+    asyncValidationAndSave();
+  }, [firstName, lastName, email, photo, form, setResumeData]);
+
+  const photoInputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Personal info</h2>
+        <p className="text-sm text-muted-foreground">Tell us about yourself.</p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          <FormField
+            control={form.control}
+            name="photo"
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            render={({ field: { value, ...fieldValues } }) => (
+              <FormItem>
+                <FormLabel>Your photo</FormLabel>
+                <div className="flex items-center gap-2">
+                  <FormControl>
+                    <Input
+                      {...fieldValues}
+                      type="file"
+                      accept="image/*"
+                      onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        fieldValues.onChange(file);
+                      }}
+                      ref={photoInputRef}
+                    />
+                  </FormControl>
+                  <Button
+                    variant="secondary"
+                    type="button"
+                    onClick={() => {
+                      fieldValues.onChange(null);
+                      if (photoInputRef.current) photoInputRef.current.value = "";
+                    }}
+                  >
+                    Remove
+                  </Button>
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-2 gap-3">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          {/* ToDo: more fields */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} type="email" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default Step2PersonalInfoForm;
