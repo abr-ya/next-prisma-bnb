05936f3b6bd3b3e75f871bcee9f7855d446ed425
abr-ya <gamer05@list.ru>
2025-05-18 15:54:11 +0300

    feature: Add zoom to ResumePreview

--- app/resumes/editor/_components/ResumePreview.tsx
+++ app/resumes/editor/_components/ResumePreview.tsx
@@ -6,6 +6,8 @@ import SummarySection from "./resumePreview/SummarySection";
 import WorkExperienceSection from "./resumePreview/WorkExperienceSection";
 import EducationSection from "./resumePreview/EducationSection";
 import SkillsSection from "./resumePreview/SkillsSection";
+import useDimensions from "@/app/_hooks/useDimensions";
+import { cn } from "@/lib/utils";
 
 interface IResumePreview {
   data: ResumeValuesType;
@@ -14,10 +16,13 @@ interface IResumePreview {
 const ResumePreview: FC<IResumePreview> = ({ data }) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
+  const { width } = useDimensions(containerRef);
+
   return (
     <div className="flex w-full justify-center overflow-y-auto bg-secondary p-3">
       <div className="max-w-2xl shadow-md aspect-[210/297] h-fit w-full bg-white text-black" ref={containerRef}>
-        <div className="space-y-6 p-6" id="resumePreviewContent">
+        {/* with zoom! */}
+        <div className={cn("space-y-6 p-6", !width && "invisible")} style={{ zoom: (1 / 794) * width }}>
           <PersonalInfoHeader data={data} />
           <SummarySection data={data} />
           <WorkExperienceSection data={data} />
