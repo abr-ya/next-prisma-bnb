26737644535c158ad69cbdb1ee2024c52252d901
abr-ya <gamer05@list.ru>
2026-05-14 12:16:52 +0400

    fix(loading): Use SkeletonList instead of placeholder text

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,9 @@
-const Loading = () => <>app == default loading.......</>;
+import SkeletonList from "./_components/SkeletonList";
+
+const Loading = () => (
+  <div className="container mx-auto px-5 lg:px-10">
+    <SkeletonList />
+  </div>
+);
 
 export default Loading;
